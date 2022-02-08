a7a636c65096b27024f059c586f9648e62b3486f
fremen432 <cbmiller4897@gmail.com>
2022-02-08 13:31:45 -0600

    miller

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Clayton!
+          This is Clayton Miller!
         </p>
         <a
           className="App-link"
